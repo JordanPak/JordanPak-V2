1ad7ff7a74d828cf3a98e51c67c1f7b6e9a0e23c
Jordan Pakrosnis <jpak@jordanpak.com>
2021-07-07 23:20:01 -0400

    Init assets handler class

---
+++ wp-content/themes/jordanpak/src/Assets.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * CSS/JS handling
+ *
+ * @since   1.0.0
+ * @package JordanPak
+ */
+
+namespace JordanPak;
+
+// exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * CSS/JS asset handling class
+ *
+ * @since 1.0.0
+ */
+class Assets {
+}
