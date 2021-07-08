7f90341feecd8df4496fffd97f2327c7733de90b
Jordan Pakrosnis <jpak@jordanpak.com>
2021-07-07 23:51:41 -0400

    Enqueue global CSS

--- wp-content/themes/jordanpak/src/Assets.php
+++ wp-content/themes/jordanpak/src/Assets.php
@@ -2,7 +2,7 @@
 /**
  * CSS/JS handling
  *
- * @since   1.0.0
+ * @since   2.0.0
  * @package JordanPak
  */
 
@@ -16,7 +16,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * CSS/JS asset handling class
  *
- * @since 1.0.0
+ * @since 2.0.0
  */
 class Assets {
+
+	/**
+	 * Asset registration handle prefix
+	 *
+	 * @since 2.0.0
+	 * @var   string
+	 */
+	const HANDLE_PREFIX = 'jordanpak-';
+
+	/**
+	 * Hook everything in
+	 *
+	 * @since 2.0.0
+	 */
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ] );
+	}
+
+	/**
+	 * Register and enqueue CSS/JS assets
+	 *
+	 * @since 2.0.0
+	 */
+	public function enqueue_assets() {
+		$build_dir = JORDANPAK_DIR . '/build';
+		$build_url = JORDANPAK_URL . '/build';
+
+		wp_enqueue_style(
+			self::HANDLE_PREFIX . 'global',
+			"$build_url/style-global.css",
+			[],
+			filemtime( "$build_dir/style-global.css" )
+		);
+	}
 }
