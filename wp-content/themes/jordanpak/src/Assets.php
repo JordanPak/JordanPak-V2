a3e48c214565d133a65ff63caafd03386298a132
Jordan Pakrosnis <jpak@jordanpak.com>
2021-07-09 00:17:47 -0400

    Get some fonts working properly

--- wp-content/themes/jordanpak/src/Assets.php
+++ wp-content/themes/jordanpak/src/Assets.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * CSS/JS handling
+ * CSS/JS/Fonts handling
  *
  * @since   2.0.0
  * @package JordanPak
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * CSS/JS asset handling class
+ * CSS/JS/Fonts asset handling class
  *
  * @since 2.0.0
  */
@@ -34,23 +34,78 @@ class Assets {
 	 * @since 2.0.0
 	 */
 	public function __construct() {
+		add_action( 'after_setup_theme', [ $this, 'add_editor_styles' ] );
+		add_action( 'init', [ $this, 'do_asset_registration' ] );
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ] );
+		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_editor_assets' ], 5000 );
 	}
 
 	/**
-	 * Register and enqueue CSS/JS assets
+	 * Add editor styles
+	 *
+	 * Required for proper editor canvas scoping.
+	 *
+	 * @since 2.7.0
+	 */
+	public function add_editor_styles() {
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'build/style-global.css' );
+	}
+
+	/**
+	 * Register CSS/JS assets
 	 *
 	 * @since 2.0.0
 	 */
-	public function enqueue_assets() {
+	public function do_asset_registration() {
 		$build_dir = JORDANPAK_DIR . '/build';
 		$build_url = JORDANPAK_URL . '/build';
 
-		wp_enqueue_style(
+		// Global styles.
+		wp_register_style(
 			self::HANDLE_PREFIX . 'global',
 			"$build_url/style-global.css",
 			[],
 			filemtime( "$build_dir/style-global.css" )
 		);
+
+		// Font loader.
+		wp_register_script(
+			self::HANDLE_PREFIX . 'font-loader',
+			'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js',
+			[],
+			'1.6.26',
+			true
+		);
+
+		wp_add_inline_script(
+			self::HANDLE_PREFIX . 'font-loader',
+			"
+				WebFont.load({ google: { families: [
+					'Roboto Condensed:400,700',
+					'Roboto:400,400i,500,700',
+					'Bebas Neue',
+				] } });
+			"
+		);
+	}
+
+	/**
+	 * Enqueue front end CSS/JS assets
+	 *
+	 * @since 2.0.0
+	 */
+	public function enqueue_assets() {
+		wp_enqueue_style( self::HANDLE_PREFIX . 'global' );
+		wp_enqueue_script( self::HANDLE_PREFIX . 'font-loader' );
+	}
+
+	/**
+	 * Enqueue editor assets
+	 *
+	 * @since 2.0.0
+	 */
+	public function enqueue_editor_assets() {
+		wp_enqueue_script( self::HANDLE_PREFIX . 'font-loader' );
 	}
 }
