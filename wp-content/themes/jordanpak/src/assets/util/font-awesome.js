f0501456f942a1fd9c9d65a0dfd5f27cdb386524
Jordan Pakrosnis <jpak@jordanpak.com>
2021-07-23 17:19:51 -0400

    install FA Pro light icons

--- wp-content/themes/jordanpak/src/assets/util/font-awesome.js
+++ wp-content/themes/jordanpak/src/assets/util/font-awesome.js
@@ -6,5 +6,7 @@
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
 
 library.add( far );
+library.add( fal );
